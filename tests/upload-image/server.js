203ce603238987f426555c1edb6e2bfec0fcc7ba
freak1n <yavor.st.m@gmail.com>
2013-04-15 16:47:45 +0300

    First complete paper from The Node beginner book

--- tests/upload-image/server.js
+++ tests/upload-image/server.js
@@ -1,21 +1,43 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	console.log("PEsho");
-	response.end();
-}).listen(8888);
+function start(route) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
 
-	// var http = require("http");
+    route(pathname);
 
-	// function onRequest(request, response) {
-	// 	response.writeHead(200, {"Content-Type": "text/pain"});
-	// 	response.write("Hello World!");
-	// 	response.end;
-	// }
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
 
-	// http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
+
+// var http = require("http");
+
+// http.createServer(function(request, response) {
+// 	response.writeHead(200, {"Content-Type": "text/plain"});
+// 	response.write("Hello World");
+// 	console.log("PEsho");
+// 	response.end();
+// }).listen(8888);
+
+
+// var http = require("http");
+
+// function onRequest(request, response) {
+// 	response.writeHead(200, {"Content-Type": "text/pain"});
+// 	response.write("Hello World!");
+// 	response.end;
+// }
+
+// http.createServer(onRequest).listen(8888);
 
 /*var http = require("http");
 
